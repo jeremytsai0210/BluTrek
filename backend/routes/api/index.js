dfe06198a269aa68abcd37fcd93151a07ab764b1
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-12-05 01:42:52 -0800

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,8 @@
 // Details: This file contains the routes for the API of the FoodTrek application. The code snippet provided is a part of the file that adds a route to test the API. The route responds with the request body as a JSON object.
 
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
@@ -10,6 +12,14 @@ const { restoreUser } = require('../../utils/auth.js');
 // These imports are for testing User Authentication Middleware purposes only.
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 
 // Test User Authentication Middleware //
 // If there is no session user, the route will return an error. Otherwise it will return the session user's information.
