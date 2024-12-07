9b1df78dc20e28c809daffb57d66d7125f1ab24e
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-12-06 23:44:51 -0800

    Updated backend for frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,71 @@
-// backend/routes/index.js
-// Purpose: This file contains the routes for the backend of the FoodTrek application.
-// Details: This file contains the routes for the backend of the FoodTrek application. The code snippet provided is a part of the file that adds a route to restore the CSRF token. The route restores the CSRF token by adding a XSRF-TOKEN cookie to the response.
+// // backend/routes/index.js
+
+// const express = require("express");
+// const router = express.Router();
+// const apiRouter = require("./api");
+// const csrf = require("csurf");
+// const csrfProtection = csrf({ cookie: true });
+
+// router.use(csrfProtection);
+
+// router.use("/api", apiRouter);
+
+// // Static routes
+// // Serve React build files in production
+// if (process.env.NODE_ENV === 'production') {
+//   const path = require('path');
+//   // Serve the frontend's index.html file at the root route
+//   router.get('/', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.sendFile(
+//       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+//     );
+//   });
+
+//   // Serve the static assets in the frontend's build folder
+//   router.use(express.static(path.resolve("../frontend/dist")));
+
+//   // Serve the frontend's index.html file at all other routes NOT starting with /api
+//   router.get(/^(?!\/?api).*/, (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.sendFile(
+//       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+//     );
+//   });
+// }
+
+// // Add a XSRF-TOKEN cookie in development
+// if (process.env.NODE_ENV !== 'production') {
+//   router.get('/api/csrf/restore', (req, res) => {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     return res.status(200).json({
+//       'XSRF-Token': csrfToken
+//     });
+//   });
+// }
+
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     "XSRF-Token": csrfToken,
+//   });
+// });
+
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
+
+// module.exports = router;
+
+////////////////////////////////////////////////////////////////////////////////
+/* 
+Above is the setup we finished with after completing the backend of the project.
+The following is the new setup asked of us when are trying to integrate the frontend
+with the backend. For the most part, it is the same, but there are a few differneces
+and I don't want to mess with anything.
+*/
+////////////////////////////////////////////////////////////////////////////////
 
 const express = require('express');
 const router = express.Router();
@@ -15,7 +80,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at the root route
   router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
@@ -26,7 +91,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
     );
   });
@@ -34,26 +99,13 @@ if (process.env.NODE_ENV === 'production') {
 
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== 'production') {
-  router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    return res.json({});
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
   });
 }
-
-/****************************** backend development ******************************/
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-// Add a XSRF-TOKEN cookie
-// router.get("/api/csrf/restore", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("XSRF-TOKEN", csrfToken);
-//   res.status(200).json({
-//     'XSRF-Token': csrfToken
-//   });
-// });
 
 module.exports = router;
