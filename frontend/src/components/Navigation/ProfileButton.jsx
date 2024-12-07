485c4e0435e96b45efabd99952df328ec17dc9af
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-12-06 17:57:44 -0800

    LoginModal completed

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,10 +1,56 @@
+// frontend/src/components/Navigation/ProfileButton.jsx
+
+import { useState, useEffect, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import { FaCircleUser } from 'react-icons/fa6';
+import * as sessionActions from '../../store/session';
 
 const ProfileButton = (user) => {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+
+        const closeMenu =() => {
+            if(ulRef.current && !ulRef.current.contains(event.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+
+    // onClick Handler for logout button
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
     return (
-        <div className="profile-button">
-            <FaCircleUser />
-        </div>
+        <>
+            <button onClick={toggleMenu}>
+                <FaCircleUser />
+            </button>
+            <ul className={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+            </ul>
+        </>
     );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import './index.css';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -21,8 +22,11 @@ if (process.env.NODE_ENV !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
