24b904c3fd0aa32b18d6f1182ff9b20c3851aac3
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-12-06 16:18:40 -0800

    Logout completed

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -18,6 +18,8 @@ const removeUser = () => {
   };
 };
 
+
+// Login
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -32,10 +34,38 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+// Restore User
 export const restoreUser = () => async (dispatch) => {
     const response = await csrfFetch("/api/session");
     const data = await response.json();
     dispatch(setUser(data.user));
+    return response;
+};
+
+// Signup
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        firstName,
+        lastName,
+        email,
+        password
+      })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+// Logout
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session", {
+      method: "DELETE"
+    });
+    dispatch(removeUser());
     return response;
 };
 
