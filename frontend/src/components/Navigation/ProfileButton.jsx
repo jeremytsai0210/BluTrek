9826d834da7f5d10510d33ced320407ff352bcec
Jeremy Tsai <jeremytsai0210@gmail.com>
2024-12-06 16:45:02 -0800

    Navigation component created

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,11 @@
+import { FaCircleUser } from 'react-icons/fa6';
+
+const ProfileButton = (user) => {
+    return (
+        <div className="profile-button">
+            <FaCircleUser />
+        </div>
+    );
+};
+
+export default ProfileButton;
